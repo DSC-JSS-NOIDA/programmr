d6f13397c6d78764b972a797d5d30851b3d44eb6
nooreenharoon <nooreenharoon@gmail.com>
2016-12-20 21:31:21 +0530

    DB changed

--- programmr/app/admin.py
+++ programmr/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Users
+from app.models import Questions
+from app.models import Submission
+
+
+admin.site.register(Users)
+admin.site.register(Questions)
+admin.site.register(Submission)
+
+
--- programmr/app/models.py
+++ programmr/app/models.py
@@ -1,5 +1,47 @@
 from __future__ import unicode_literals
+from django.conf import settings
 
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+	name=models.CharField(max_length=120)
+	email_ID=models.CharField(max_length=200)
+	avatar=models.URLField(max_length=120)
+	year=models.CharField(max_length=120)
+	branch=models.CharField(max_length=120)
+	mobile_no=models.CharField(max_length=120)
+	timestamp=models.DateTimeField(auto_now=True,auto_now_add=False)
+	total_score=models.CharField(max_length=120)
+
+
+class Questions(models.Model):
+	title=models.CharField(max_length=120)
+	detail=models.TextField()
+	constraint=models.CharField(max_length=120)
+	input_format=models.TextField()
+	output_format=models.TextField()
+	sample_testcase=models.TextField()
+	testcase_input=models.TextField()
+	testcase_output=models.TextField()
+
+
+class Submission(models.Model):
+	user_ID=models.CharField(max_length=120)
+	question_ID=models.CharField(max_length=120)
+	status=models.CharField(max_length=120)
+	source_code_URL=models.URLField(max_length=120)
+	date=models.DateTimeField(auto_now=True,auto_now_add=False)
+
+
+def __unicode__(self):
+	return self.name
+
+
+
+
+	
+
+
+
