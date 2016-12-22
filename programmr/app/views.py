fd53685ab908e44d3c832c1f6947a875dab8fcfd
sehgalayush1 <sehgalayush26@gmail.com>
2016-12-22 22:27:36 +0530

    google login api updated1

--- programmr/app/views.py
+++ programmr/app/views.py
@@ -4,13 +4,13 @@ from django import http
 from oauth2client.contrib.django_util import decorators
 
 # Create your views here.
-def index(request):
-	return http.HttpResponse("Hello, World!")
+def login(request):
+	return render(request, 'login.html')
 
 
 
 @decorators.oauth_required
-def get_profile_required(request):
+def google_login(request):
 	resp, content = request.oauth.http.request('https://www.googleapis.com/plus/v1/people/me')
 	return http.HttpResponse(content)
 
