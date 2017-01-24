bf50caeff83fbf3165d6a3df7e0e49d96500b1a0
sehgalayush1 <sehgalayush26@gmail.com>
2017-01-24 23:03:16 +0530

    Admin Panel Edited

--- programmr/app/admin.py
+++ programmr/app/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 from app.models import *
 
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	model = UserProfile
+	list_display = ('name', 'email_ID', 'mobile_no')
+
+class SubmissionAdmin(admin.ModelAdmin):
+	model = Submission
+	list_display = ('ques_ID', 'user_ID', 'status', 'source_code_URL')
+	list_filter = ['status']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(GoogleProfile)
 admin.site.register(Question)
-admin.site.register(Submission)
+admin.site.register(Submission, SubmissionAdmin)
 admin.site.register(Announcement)
